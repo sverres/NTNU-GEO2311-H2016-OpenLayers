b01345f83db9e3a5066182d190830ababb189bd9
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2016-04-04 20:25:44 +0200

    WMS-zoom

--- WMS-zoom/wms.js
+++ WMS-zoom/wms.js
@@ -1,7 +1,7 @@
 /*
  * http://github.com/sverres
  *
- * sverre.stikbakke 03.04.2016
+ * sverre.stikbakke 04.04.2016
  *
  */
 
@@ -9,6 +9,7 @@
 http://kartverket.no/Kart/Gratis-kartdata/WMS-tjenester/
 http://status.kartverket.no/tjenester/openwms.py?
 http://openwms.statkart.no/skwms1/wms.topo2?request=GetCapabilities&Service=WMS
+http://wms.geonorge.no/kr/koordsys_res.txt
 */
 
 var attribution = new ol.Attribution({
@@ -21,6 +22,28 @@ var projection = new ol.proj.Projection({
   code: 'EPSG:25832',
   extent: extentKartverketWMS25832
 });
+
+var resolutionsKartverket = [
+  21664,
+  10832,
+  5416,
+  2708,
+  1354,
+  677,
+  338.5,
+  169.25,
+  84.625,
+  42.3125,
+  21.15625,
+  10.578125,
+  5.2890625,
+  2.64453125,
+  1.322265625,
+  0.6611328125,
+  0.33056640625,
+  0.165283203125,
+  0.0826416015625
+];
 
 var topo2 = new ol.layer.Tile({
   extent: extentKartverketWMS25832,
@@ -40,8 +63,7 @@ var map = new ol.Map({
   view: new ol.View({
     projection: projection,
     center: [591500, 6740500],
-    minResolution: 2,
-    maxResolution: 200,
-    resolution: 20,
+    resolutions: resolutionsKartverket,
+    zoom: 15
   })
 });
